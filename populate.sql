8efe22f5b3926195d0968c7789410c00325b5bcd
Daniela Gorjão Lopes <ist186403@lab6p1.localdomain>
2018-11-22 13:20:21 +0000

    corrected errors

---
+++ populate.sql
@@ -0,0 +1,123 @@
+----------------------------------------
+-- Populate Relations
+----------------------------------------
+
+/*camara*/
+insert into camara values (1);
+insert into camara values (2);
+insert into camara values (3);
+insert into camara values (4);
+
+/*video*/
+/*insert into video values ('Adams',	'Main Street',	'Lisbon');
+insert into video values ('Brown',	'Main Street',	'Oporto');
+insert into video values ('Cook',	'Main Street',	'Lisbon');
+insert into video values ('Davis',	'Church Street','Oporto');*/
+
+/*segmentoVideo*/
+/*insert into segmentoVideo values ('Downtown',	'Lisbon',		1900000);
+insert into segmentoVideo values ('Central',	'Cascais',		2100000);
+insert into segmentoVideo values ('Uptown',	'Amadora',		1700000);
+insert into segmentoVideo values ('Metro',	'Amadora',	 	400200);*/
+
+/*local*/
+/*insert into local values ('A-101',	'Downtown',	500);
+insert into local values ('A-215',	'Metro',	600);
+insert into local values ('A-102',	'Uptown',	700);
+insert into local values ('A-305',	'Round Hill',	800);*/
+
+/*vigia*/
+/*insert into vigia values ('Johnson', 'A-101');
+insert into vigia values ('Brown',	 'A-215');
+insert into vigia values ('Cook',	 'A-102');
+insert into vigia values ('Cook',	 'A-101');*/
+
+/*eventoEmergencia*/
+/*insert into eventoEmergencia values ('L-17', 'Downtown',	1000);
+insert into eventoEmergencia values ('L-23', 'Central',	2000);
+insert into eventoEmergencia values ('L-15', 'Uptown',	3000);
+insert into eventoEmergencia values ('L-14', 'Downtown',	4000);*/
+
+/*processoSocorro*/
+/*insert into processoSocorro values ('Iacocca',	'L-17');
+insert into processoSocorro values ('Brown',	'L-23');
+insert into processoSocorro values ('Cook',	'L-15');
+insert into processoSocorro values ('Nguyen',	'L-14');
+insert into processoSocorro values ('Davis',	'L-93');*/
+
+/*entidadeMeio*/
+/*insert into entidadeMeio values ('Iacocca',	'L-17');
+insert into entidadeMeio values ('Brown',	'L-23');
+insert into entidadeMeio values ('Cook',	'L-15');
+insert into entidadeMeio values ('Nguyen',	'L-14');
+insert into entidadeMeio values ('Davis',	'L-93');*/
+
+/*meio*/
+/*insert into meio values ('Iacocca',	'L-17');
+insert into meio values ('Brown',	'L-23');
+insert into meio values ('Cook',	'L-15');
+insert into meio values ('Nguyen',	'L-14');
+insert into meio values ('Davis',	'L-93');*/
+
+/*meioCombate*/
+/*insert into meioCombate values ('Iacocca',	'L-17');
+insert into meioCombate values ('Brown',	'L-23');
+insert into meioCombate values ('Cook',	'L-15');
+insert into meioCombate values ('Nguyen',	'L-14');
+insert into meioCombate values ('Davis',	'L-93');*/
+
+/*meioApoio*/
+/*insert into meioApoio values ('Iacocca',	'L-17');
+insert into meioApoio values ('Brown',	'L-23');
+insert into meioApoio values ('Cook',	'L-15');
+insert into meioApoio values ('Nguyen',	'L-14');
+insert into meioApoio values ('Davis',	'L-93');*/
+
+/*meioSocorro*/
+/*insert into meioSocorro values ('Iacocca',	'L-17');
+insert into meioSocorro values ('Brown',	'L-23');
+insert into meioSocorro values ('Cook',	'L-15');
+insert into meioSocorro values ('Nguyen',	'L-14');
+insert into meioSocorro values ('Davis',	'L-93');*/
+
+/*transporta*/
+/*insert into transporta values ('Iacocca',	'L-17');
+insert into transporta values ('Brown',	'L-23');
+insert into transporta values ('Cook',	'L-15');
+insert into transporta values ('Nguyen',	'L-14');
+insert into transporta values ('Davis',	'L-93');*/
+
+/*alocado*/
+/*insert into alocado values ('Iacocca',	'L-17');
+insert into alocado values ('Brown',	'L-23');
+insert into alocado values ('Cook',	'L-15');
+insert into alocado values ('Nguyen',	'L-14');
+insert into alocado values ('Davis',	'L-93');*/
+
+/*acciona*/
+/*insert into acciona values ('Iacocca',	'L-17');
+insert into acciona values ('Brown',	'L-23');
+insert into acciona values ('Cook',	'L-15');
+insert into acciona values ('Nguyen',	'L-14');
+insert into acciona values ('Davis',	'L-93');*/
+
+/*coordenador*/
+/*insert into coordenador values ('Iacocca',	'L-17');
+insert into coordenador values ('Brown',	'L-23');
+insert into coordenador values ('Cook',	'L-15');
+insert into coordenador values ('Nguyen',	'L-14');
+insert into coordenador values ('Davis',	'L-93');*/
+
+/*audita*/
+/*insert into audita values ('Iacocca',	'L-17');
+insert into audita values ('Brown',	'L-23');
+insert into audita values ('Cook',	'L-15');
+insert into audita values ('Nguyen',	'L-14');
+insert into audita values ('Davis',	'L-93');*/
+
+/*solicita*/
+/*insert into solicita values ('Iacocca',	'L-17');
+insert into solicita values ('Brown',	'L-23');
+insert into solicita values ('Cook',	'L-15');
+insert into solicita values ('Nguyen',	'L-14');
+insert into solicita values ('Davis',	'L-93');*/
